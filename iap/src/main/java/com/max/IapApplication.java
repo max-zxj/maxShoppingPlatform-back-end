4ca3c85dbf2025cf920095aa22a1ab8d7b38317f
max-zxj <972458547@qq.com>
2023-02-16 15:06:31 -0500

    Built the framework of project and configured dependencies

---
+++ iap/src/main/java/com/max/IapApplication.java
@@ -0,0 +1,14 @@
+package com.max;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+//@MapperScan("com.max.xzmall.dao")
+public class IapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IapApplication.class, args);
+    }
+
+}
